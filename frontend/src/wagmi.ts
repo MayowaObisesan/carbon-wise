50dc31b0312e2d9ff192f87d5cd64961aa1c816e
MAC <mac@koniesJay.local>
2024-09-27 15:34:26 +0100

    integrated lisk in wagmi.ts

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,37 +1,33 @@
 import { configureChains, createConfig } from "wagmi";
-import { goerli, mainnet, sepolia, baseGoerli } from "wagmi/chains";
-import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
-import { InjectedConnector } from "wagmi/connectors/injected";
+import { liskSepolia } from "viem/chains";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
-    mainnet,
-    sepolia,
-    baseGoerli,
-    ...(import.meta.env?.MODE === "development" ? [goerli] : []),
+    liskSepolia,
+    ...(import.meta.env?.MODE === "development" ? [liskSepolia] : []),
   ],
   [
     alchemyProvider({ apiKey: "ix0-fxmVivwaIWYHtIpwVZB7wC8TpxEm" }),
     publicProvider(),
   ]
 );
-console.log(baseGoerli);
+
+console.log(liskSepolia);
 
 export const config = createConfig({
   autoConnect: true,
   connectors: [
     new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: "wagmi",
-      },
-    }),
+    // new CoinbaseWalletConnector({
+    //   chains,
+    //   options: {
+    //     appName: "wagmi",
+    //   },
+    // }),
     // new InjectedConnector({
     //   chains,
     //   options: {
